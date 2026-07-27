7bee14b7c2d3aa87ca59979f2bf346b607bd3257
Leoni <leoni.dessau@mailbox.org>
2026-07-27 19:45:46 +0200

    asdf

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from sciebo_get import get_sciebo_directory
-from sciebo_get import fetch
+from sciebo_fetch.sciebo_get import get_sciebo_directory
+from sciebo_fetch.sciebo_get import fetch
--- sciebo_get.py
+++ sciebo_get.py
@@ -3,14 +3,33 @@ from urllib import request
 import tempfile
 
 
+class content:
+    def __init__(self, zip: zipfile.Zipfile):
+        self.zip = zip
+
+    def ls(self) -> [str]:
+        all_files = self.zip.namelist()
+        not_dir = filter(lambda f: not self.zip.getinfo(f).is_dir(), all_files)
+        return list(not_dir)
+
+    def read_file(self, file: str) -> bytes:
+        with self.zip.open(file) as handle:
+            return handle.read()
+
+    def open(self, file: str):
+        return self.zip.open(file)
+
+
 def get_sciebo_directory(url: str, cache_locally=False) -> zipfile.ZipFile:
     response = request.urlopen(url)
     storage = tempfile.TemporaryFile()
     storage.write(response.read())
     storage.seek(0)
     zf = zipfile.ZipFile(storage)
-    print(zf.namelist())
-    return None
+    # print(zf.namelist())
+    return zf
 
 
-fetch = get_sciebo_directory
+def fetch(url: str):
+    return content(get_sciebo_directory(url))
+    
