53b026d5a671b0eb52c2248503e1d1bf29bb44d9
Leoni <leoni.dessau@mailbox.org>
2026-07-29 17:03:17 +0200

    adf

--- sciebo_get.py
+++ sciebo_get.py
@@ -33,7 +33,7 @@ def get_sciebo_directory(url: str, cache_locally=False) -> zipfile.ZipFile:
         storage = tempfile.TemporaryFile()
         storage.write(response.read())
     else:
-        cache_file = open(cache_locally, "w")
+        cache_file = open(cache_locally, "wb")
         cache_file.write(response.read())
         cache_file.close()
         storage = open(cache_locally, "r")
