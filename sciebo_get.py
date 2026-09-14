52fc6d9374f7c18063e7a01e8fe3495c315d09bb
Leoni <leoni.dessau@mailbox.org>
2026-09-14 15:53:11 +0200

    aadf

--- sciebo_get.py
+++ sciebo_get.py
@@ -45,7 +45,9 @@ def get_sciebo_directory(url: str, cache_locally=False) -> zipfile.ZipFile:
 
 def fetch(url: str, cache_as=False, force_load=False):
     if cache_as:
-        url_hash = hashlib.md5().update(url.encode()).hexdigest()
+        md5_hash = hashlib.md5()
+        md5_hash.update(url.encode())
+        url_hash = md5_hash.hexdigest()
         temp_path = tempfile.gettempdir() + "sciebo_cache_" + url_hash
         if len(glob.glob(temp_path)) > 0 and not force_load:
             print("caching and found")
